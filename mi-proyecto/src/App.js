72741e1ff6f32fcbf460062710a4ed1b00908448
JuanPugliese <jcp5432@gmail.com>
2025-08-13 11:25:47 -0500

    Accesibilidad + seo

--- mi-proyecto/src/App.js
+++ mi-proyecto/src/App.js
@@ -1,4 +1,7 @@
+// src/App.js
 import React from 'react';
+import './styles/global.css';
+
 import MyNavbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
@@ -6,18 +9,28 @@ import Services from './components/Services';
 import ContactInfo from './components/ContactInfo';
 import Footer from './components/Footer';
 
-function App() {
+export default function App() {
   return (
     <>
-      <MyNavbar />
-      <Hero />
-      <About /> 
-      <Services /> 
-      <ContactInfo/>
-      <Footer />
+      {/* Permite saltar al contenido con teclado */}
+      <a href="#main" className="skip-link">Skip to content</a>
+
+      <header>
+        <MyNavbar />
+      </header>
+
+      {/* tabIndex para que el foco aterrice aquí al usar el skip link */}
+      <main id="main" tabIndex="-1">
+        {/* Hero debe contener el ÚNICO <h1> del sitio */}
+        <Hero />
+        <About />
+        <Services />
+        <ContactInfo />
+      </main>
+
+      <footer>
+        <Footer />
+      </footer>
     </>
   );
 }
-
-export default App;
-
